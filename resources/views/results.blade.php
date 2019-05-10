8820ec4adeb8ed6346fc055e595e00fa6324bd84
Tosin Adenaike <tossie79@yahoo.ca>
2019-05-10 20:24:42 +0100

    Fixed some issues

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -1,19 +1,19 @@
 @extends('layouts.master')
 @section('title')
-Calculator App
+	Calculator App
 @endsection
 @section('content')
-<div class="row">
-    <div class="col-md-8 col-md-offset-4">
-        <h1>Calculator App Results</h1>
-            <h2>
-                {{$resultDetails['number1']}}
-                {{$resultDetails['operator']}}
-                {{$resultDetails['number2']}}
-                =
-                {{round($resultDetails['results'],2)}}
-            </h2>
-            <a type="button"  class='btn btn-primary' href="{{route('index')}}" title="Click to Go Back">Click To Go Back To Calculator Page</a>
-    </div>
-</div>
+	<div class="row">
+		<div class="col-md-8 col-md-offset-4">
+			<h1>Calculator App Results</h1>
+				<h2>
+					{{$resultDetails['number1']}}
+					{{$resultDetails['operator']}}
+					{{$resultDetails['number2']}}
+					=
+					{{round($resultDetails['results'],2)}}
+				</h2>
+				<a type="button"  class='btn btn-primary' href="{{route('index')}}" title="Click to Go Back">Click To Go Back To Calculator Page</a>
+		</div>
+	</div>
 @endsection
